cacb7d9c131a9c6bf2d3c3e9ee40657436977205
Aleksander Żulewski <azulewski@gmail.com>
2020-05-24 21:22:59 +0200

    Added form to add a user to the database

--- src/main/java/com/thymeleaf/learn/Thymeleaf/api/user/UserController.java
+++ src/main/java/com/thymeleaf/learn/Thymeleaf/api/user/UserController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +24,22 @@ public class UserController {
         List<User> users = userService.findAll();
 
         model.addAttribute("users", users);
+
+        return "users/list";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "users/form";
+    }
+
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.save(user);
 
         return "users/list";
     }
